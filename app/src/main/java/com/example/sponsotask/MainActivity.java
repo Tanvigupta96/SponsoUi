b8ed25d75df0550609b962e711f568bf27ec62c8
Tanvigupta96 <guptatannu.34gmail.com>
2020-01-08 20:10:07 +0530

    added toolbar

--- app/src/main/java/com/example/sponsotask/MainActivity.java
+++ app/src/main/java/com/example/sponsotask/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.sponsotask;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import androidx.appcompat.widget.Toolbar;
-
-
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  BottomNavigationView.OnNavigationItemSelectedListener {
     Toolbar toolbar;
     FrameLayout frameLayout;
-    BottomNavigationMenu bottomNavigationView;
+    BottomNavigationView bottomNavigationView;
     CollapsingToolbarLayout collapsingToolbarLayout;
+    HomeFragment homeFragment=new HomeFragment();
+    LiveClasses liveClasses=new LiveClasses();
+    DownloadsFragment downloadsFragment=new DownloadsFragment();
+    HelpFragment helpFragment=new HelpFragment();
+    FragmentManager manager1 = getSupportFragmentManager();
+    FragmentTransaction transaction1 = manager1.beginTransaction();
 
 
 
@@ -28,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         frameLayout = findViewById(R.id.container);
@@ -36,10 +46,54 @@ public class MainActivity extends AppCompatActivity {
         collapsingToolbarLayout.setTitleEnabled(false);
         getSupportActionBar().setTitle(" ");
 
-
-
-
-
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+
+        switch (menuItem.getItemId()) {
+            case R.id.Home:
+                getSupportActionBar().setTitle("Home");
+                transaction.replace(R.id.container, homeFragment);
+
+
+                break;
+
+            case R.id.LiveClasses:
+                getSupportActionBar().setTitle("Live Classes");
+
+                transaction.replace(R.id.container, liveClasses);
+
+                break;
+
+            case R.id.Downloads:
+                getSupportActionBar().setTitle("Downloads");
+
+                transaction.replace(R.id.container, downloadsFragment);
+                break;
+
+            case R.id.Help:
+                getSupportActionBar().setTitle("Help");
+
+                transaction.replace(R.id.container,helpFragment);
+                break;
+
+
+        }
+        transaction.commit();
+
+        return true;
+    }
+
+
+
+
+
+
+
 }
